decd9b0c5ebe8cc8d9749cf05192bc77d75740f5
Randy Houlahan <randy.houlahan@cbd.int>
2017-08-15 10:48:13 -0400

    remove duplicate interceptor

--- app/app.js
+++ app/app.js
@@ -34,26 +34,6 @@ define(['angular'], function(angular) { 'use strict';
     });
   }]);
 
-    app.factory('realmHttpIntercepter', ["realm", function(realm) {
-
-		return {
-			request: function(config) {
-				var trusted = /^http:\/\/localhost\//i .test(config.url)||
-                              /^https:\/\/api.cbd.int\//i .test(config.url) ||
-							  /^\/api\//i                 .test(config.url);
-
-                //exception if the APi call needs to be done for different realm
-                if(trusted && realm && config.params && config.params.realm && config.params.realm != realm) {
-                      config.headers = angular.extend(config.headers || {}, { realm : config.params.realm });
-                }
-                else if(trusted && realm ) {
-                    config.headers = angular.extend(config.headers || {}, { realm : realm });
-                }
-
-                return config;
-			}
-		};
-	}]);
 
     return app;
 });
