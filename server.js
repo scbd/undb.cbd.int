44141327fa61c8f0780bebe3badf2155664a7778
Stephane <bilodeaustephane@gmail.com>
2021-12-21 17:32:56 -0500

    Use base_path from env vars

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
+const path      = require('path');
 var express     = require('express');
 var httpProxy   = require('http-proxy');
 
 // Create server & proxy
 var app    = express();
 var proxy  = httpProxy.createProxyServer({});
+const basePath = path.join('/', process.env.BASE_PATH||'/', '/');
 
-if(!process.env.API_URL) {
-    console.warn('warning: evironment API_URL not set. USING default (https://api.cbddev.xyz:443)');
-}
+if(!process.env.API_URL)   console.error(`WARNING: environment API_URL not set. USING default`);
+if(!process.env.BASE_PATH) console.error(`WARNING: environment BASE_PATH not set. USING default`);
 
 var apiUrl = process.env.API_URL || 'https://api.cbddev.xyz:443';
 var gitVersion = (process.env.COMMIT || 'UNKNOWN').substr(0, 7);
@@ -15,24 +16,28 @@ var gitVersion = (process.env.COMMIT || 'UNKNOWN').substr(0, 7);
 console.info(`info: www.cbd.int/2011-2020`);
 console.info(`info: Git version: ${gitVersion}`);
 console.info(`info: API address: ${apiUrl}`);
-
+console.info(`info: Base Path:   ${basePath}`);
 
 app.set('views', __dirname + '/app');
 app.set('view engine', 'ejs');
 
+app.all('/api/*', function(req, res) { proxy.web(req, res, { target: apiUrl, secure: false, changeOrigin:true } ); } );
+
 // CONFIGURE /APP/* ROUTES
 
-app.use('/favicon.png',   express.static(__dirname + '/app/images/favicon.png', { maxAge: 24*60*60*1000 }));
-app.use('/app',           express.static(__dirname + '/app',                    { setHeaders: setCustomCacheControl }));
+const appRoutes = new express.Router();
 
+appRoutes.use('/favicon.png',   express.static(__dirname + '/app/images/favicon.png', { maxAge: 24*60*60*1000 }));
+appRoutes.use('/app',           express.static(__dirname + '/app',                    { setHeaders: setCustomCacheControl }));
+appRoutes.use('/app/libs',      express.static(__dirname + '/node_modules/@bower_components', { setHeaders: setCustomCacheControl }));
+appRoutes.get('/*',            function(req, res) { res.render('template', { baseUrl: basePath, gitVersion: gitVersion }); });
+appRoutes.all('/*',            function(req, res) { res.status(404).send(); } );
 
-// CONFIGURE TEMPLATE
-app.all('/api/*', function(req, res) { proxy.web(req, res, { target: apiUrl, secure: false, changeOrigin:true } ); } );
-app.use('/app/libs',      express.static(__dirname + '/node_modules/@bower_components', { setHeaders: setCustomCacheControl }));
-app.get('/*',            function(req, res) { res.render('template', { baseUrl: req.headers.base_url || '/', gitVersion: gitVersion }); });
-
-app.all('/*',            function(req, res) { res.status(404).send(); } );
 // START SERVER
+
+app.use(basePath, appRoutes);
+
+if(basePath!='/') app.use('/', appRoutes); // temps support for transition to Traefik 2 / AWS ALB
 
 app.listen(process.env.PORT || 2020, function () {
 	console.log('Server listening: %j', this.address());
